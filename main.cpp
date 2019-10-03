f6cc4f215893f20c0f93efd1b20c270fd3ca880f
d3dx13 <zhidkov2846@gmail.com>
2019-10-03 09:46:43 +0300

    первый коммит

---
+++ main.cpp
@@ -0,0 +1,129 @@
+// # USE Skillet 
+#include <sstream>
+#include <fstream>
+#include <codecvt>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <iterator>
+#include <set>
+
+using namespace std;
+
+int shingleLen; // длина шингла
+
+string tolower_ru(string str){
+  string low [] = {"а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф"," х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"};
+  string up [] = {"А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"};
+  for (int i = 0; i < 33; i++){
+    int point = str.find(up[i]);
+    while (point != -1){
+      str.replace(point, up[i].length(), low[i]);
+      point = str.find(up[i]);
+    }
+  }
+  locale loc;
+  string result = "";
+  for (string::size_type i=0; i<str.length(); ++i)
+    result += tolower(str[i], loc);
+  return result;
+}
+
+unsigned int CRC32(string source){
+  unsigned char *buf = reinterpret_cast<unsigned char*>(const_cast<char*>(source.c_str()));
+  unsigned long len = source.length();
+	unsigned long crc_table[256];
+	unsigned long crc;
+	for (int i = 0; i < 256; i++)
+	{
+		crc = i;
+		for (int j = 0; j < 8; j++)
+			crc = crc & 1 ? (crc >> 1) ^ 0xEDB88320UL : crc >> 1;
+		crc_table[i] = crc;
+	};
+	crc = 0xFFFFFFFFUL;
+	while (len--)
+	  crc = crc_table[(crc ^ *buf++) & 0xFF] ^ (crc >> 8);
+	return crc ^ 0xFFFFFFFFUL;
+}
+
+string readFile(const char* filename)
+{
+    wifstream wif(filename);
+    wif.imbue(locale(locale::empty(), new codecvt_utf8<wchar_t>));
+    wstringstream wss;
+    wss << wif.rdbuf();
+    return wss.str();
+}
+
+vector<string> canonize(string source){
+  char stop_symbols[] = ".,!?:;-\n\r()";
+  string stop_words[] = {
+    "это", "как", "так", 
+    "и", "в", "над",
+    "к", "до", "не",
+    "на", "но", "за",
+    "то", "с", "ли",
+    "а", "во", "от",
+    "со", "для", "о",
+    "же", "ну", "вы",
+    "бы", "что", "кто",
+    "он", "она"
+  }; // Вводятся быть в нижнем регистре
+  source = tolower_ru(source);
+  source += " "; // Для корректного разделения на слова
+  for (char i : stop_symbols){
+    int point = source.find(i);
+    while (point != -1){
+      source.replace(source.find(i), 1, " ");
+      point = source.find(i);
+    }
+  }
+  for (string i : stop_words){
+    i = " "+i+" ";
+    int point = source.find(i);
+    while (point != -1){
+      source.replace(point, i.length(), " ");
+      point = source.find(i);
+    }
+  }
+  vector<string>result;
+  string buffer = ""; 
+  for(int i = 0; i < source.size(); i++){
+    if(source[i] != ' '){
+      buffer += source[i];     
+    }
+    else if (buffer.size() > 0){
+      result.push_back(buffer);
+      buffer = "";
+    }
+  }
+  return result;
+}
+
+multiset<unsigned int> genshingle(vector<string> source){
+  multiset<unsigned int> result;
+  for (int i = 0; i <= source.size() - shingleLen; i++){
+    string temp = source[i];
+    for (int j = i+1; j < i + shingleLen; j++)
+      temp += " " + source[j];
+    result.insert(CRC32(temp));
+  }
+  return result;
+}
+
+double compaire(multiset<unsigned int>cmp1, multiset<unsigned int>cmp2){
+  multiset<unsigned int>intersection;
+  set_intersection(cmp1.begin(), cmp1.end(), cmp2.begin(), cmp2.end(), inserter(intersection, intersection.begin()));
+  return 200.0*intersection.size()/(cmp1.size() + cmp2.size());
+}
+
+int main() {
+  shingleLen = 3; //(слов в сообщении) >= длина шингла > 0
+  setlocale(LC_ALL, "Russian");
+  string a = readFile("text1.txt");
+  string b = readFile("text2.txt");
+  cout << compaire(genshingle(canonize(a)), genshingle(canonize(b))) << " %";
+}
